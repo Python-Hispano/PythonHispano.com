ae5f5bdc0a3fe9eb219029f853e3ae4c2cdecf31
M4RC0Sx <marcos3as3@gmail.com>
2025-09-27 11:33:57 +0200

    feat: sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://pythonhispano.com",
+  generateRobotsTxt: true, // (Opcional) Genera un robots.txt también
+  // ...otras opciones
+};
